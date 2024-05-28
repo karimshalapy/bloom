90366c9bf9a244c226608a1be7e1f5dd8e44a469
Karim Shalapy <karimshalapy@gmail.com>
2024-05-29 00:48:25 +0300

    feat: add reusable base button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,86 @@
+import clsx from "clsx";
+import {
+  ButtonHTMLAttributes,
+  DetailedHTMLProps,
+  ReactNode,
+  forwardRef,
+} from "react";
+import classes from "./Button.module.css";
+import { ArrowClockwise } from "@phosphor-icons/react";
+
+/** Represents the base size for the button. */
+export const ButtonSize = {
+  /** Represents the base size for the button.*/
+  base: "base",
+  /** Represents a larger size for the button.*/
+  lg: "lg",
+  /** Represents a smaller size for the button.*/
+  sm: "sm",
+} as const;
+
+export type ButtonSize = (typeof ButtonSize)[keyof typeof ButtonSize];
+
+/** An enum-like object representing the different variants available for a button */
+export const ButtonVariant = {
+  /**  Represents a solid button with a filled background. */
+  solid: "solid",
+  /**  Represents a ghost button with a transparent background and border. */
+  ghost: "ghost",
+  /**  Represents an outlined button with a border and no background. */
+  outlined: "outlined",
+} as const;
+export type ButtonVariant = (typeof ButtonVariant)[keyof typeof ButtonVariant];
+
+export interface ButtonProps
+  extends DetailedHTMLProps<
+    ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  > {
+  size?: ButtonSize;
+  variant?: ButtonVariant;
+  loading?: boolean;
+  slotPrefix?: ReactNode;
+  slotSuffix?: ReactNode;
+  square?: boolean;
+}
+
+/**
+ * The base button component in the app, that can be rendered as a button or link.
+ * It renders all the different variants with their divergents and sizes.
+ * @component
+ */
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  (props, reference) => {
+    const {
+      className,
+      size = ButtonSize.base,
+      variant = ButtonVariant.solid,
+      square,
+      children,
+      slotPrefix,
+      slotSuffix,
+      loading = false,
+      ...attributes
+    } = props;
+
+    return (
+      <button
+        ref={reference}
+        type="button"
+        className={clsx(
+          classes.button,
+          classes[variant],
+          classes[size],
+          square && classes.square,
+          className
+        )}
+        data-testid="button"
+        {...attributes}
+      >
+        {loading ? <ArrowClockwise className={classes.loader} /> : slotPrefix}
+        {children}
+        {slotSuffix}
+      </button>
+    );
+  }
+);
