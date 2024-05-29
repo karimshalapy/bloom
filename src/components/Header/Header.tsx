7e69aaeb038bcc7180df86f7d740b7915d9d40fe
Karim Shalapy <karimshalapy@gmail.com>
2024-05-30 00:10:02 +0300

    feat: add reusable header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,36 @@
+import { FC } from "react";
+import classes from "./Header.module.css";
+import LogoWithText from "@/assets/logo-with-text.svg";
+import Logo from "@/assets/logo.svg";
+import { Button, ButtonVariant } from "../Button/Button";
+import { ShoppingCart } from "@phosphor-icons/react";
+
+/**
+ * Header component to display the main header of the application.
+ * @component
+ */
+export const Header: FC = () => {
+  return (
+    <header className={classes.header}>
+      <div aria-hidden />
+      <img
+        className={classes.logoWithText}
+        src={LogoWithText}
+        alt="Bloom Logo"
+        draggable={false}
+      />
+      <img
+        className={classes.logo}
+        src={Logo}
+        alt="Bloom Logo"
+        draggable={false}
+      />
+      {/* TODO: add cart functionality */}
+      <Button variant={ButtonVariant.ghost} square>
+        <ShoppingCart size={24} />
+      </Button>
+    </header>
+  );
+};
+
+export default Header;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export * from "./Button/Button";
+export * from "./Header/Header";
 export * from "./Price/Price";
 export * from "./Toast/Toast";
 export * from "./ToastGroup/ToastGroup";
