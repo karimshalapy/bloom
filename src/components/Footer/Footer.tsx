00d58b44b513174a33be2af577d6376c05d802d6
Karim Shalapy <karimshalapy@gmail.com>
2024-05-31 01:37:35 +0300

    feat: add storybook link in the footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import classes from "./Footer.module.css";
 import chromaticImgSrc from "@/assets/chromatic.svg";
+import storybookImgSrc from "@/assets/storybook.svg";
 
 /**
  * Footer component for the website.
@@ -24,18 +25,38 @@ export const Footer: FC = () => {
         </a>
         . All rights reserved &copy; {new Date().getFullYear()}.
       </small>
-      <a
-        href="https://www.chromatic.com/library?appId=66575276813412991c16a5b9&branch=main"
-        title="Storybook Chromatic Library"
-        rel="noreferrer"
-        target="_blank"
-      >
-        <img
-          className={classes.chromaticLogo}
-          src={chromaticImgSrc}
-          alt="Chromatic Logo"
-        />
-      </a>
+      <nav aria-label="Chromatic external links">
+        <ul className={classes.logosWrapper}>
+          <li>
+            <a
+              href="https://main--66575276813412991c16a5b9.chromatic.com/"
+              title="Storybook Chromatic Library"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <img
+                className={classes.logo}
+                src={storybookImgSrc}
+                alt="Storybook Logo"
+              />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.chromatic.com/library?appId=66575276813412991c16a5b9&branch=main"
+              title="Storybook Chromatic Library"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <img
+                className={classes.logo}
+                src={chromaticImgSrc}
+                alt="Chromatic Logo"
+              />
+            </a>
+          </li>
+        </ul>
+      </nav>
     </footer>
   );
 };
