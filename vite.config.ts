365b1ae547b86df4d34fef6615470a3c588144cc
Karim Shalapy <karimshalapy@gmail.com>
2024-05-31 12:40:53 +0300

    ci: utilize vite mode to detect gh pages environment

--- vite.config.ts
+++ vite.config.ts
@@ -3,35 +3,32 @@
 import alias from "@rollup/plugin-alias";
 import react from "@vitejs/plugin-react";
 import { resolve } from "node:path";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 
 const projectRootDir = resolve(__dirname);
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-  return {
-    base: process.env.NODE_ENV === "gh-pages" ? "/bloom/" : "/",
-    plugins: [
-      alias({
-        entries: [
-          {
-            find: "@",
-            replacement: resolve(projectRootDir, "src"),
-          },
-        ],
-      }),
-      react(),
-    ],
-    test: {
-      environment: "jsdom",
-      globals: false,
-      setupFiles: ["./src/tests/setup.ts"],
-      coverage: {
-        enabled: true,
-        provider: "istanbul",
-        exclude: ["src/stores", "src/mocks"],
-      },
+export default defineConfig(({ mode }) => ({
+  base: mode === "gh-pages" ? "/bloom/" : "/",
+  plugins: [
+    alias({
+      entries: [
+        {
+          find: "@",
+          replacement: resolve(projectRootDir, "src"),
+        },
+      ],
+    }),
+    react(),
+  ],
+  test: {
+    environment: "jsdom",
+    globals: false,
+    setupFiles: ["./src/tests/setup.ts"],
+    coverage: {
+      enabled: true,
+      provider: "istanbul",
+      exclude: ["src/stores", "src/mocks"],
     },
-  };
-});
+  },
+}));
